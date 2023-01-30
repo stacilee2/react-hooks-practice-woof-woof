f89d7e3decd5112f6a59dfa0b02c94d9b0936a98
Staci Rosales <stacileep2@gmail.com>
2023-01-30 10:51:27 -0600

    commit tree 1fb0132743444d23f214432c5d9fca0cdbf5c7af

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 function App() {
+
+  fetch("http://localhost:3000/dogs")
+  .then (res => res.json())
+  .then (dogs => console.log(dogs))
+
+
+
   return (
     <div className="App">
       <div id="filter-div">
         <button id="good-dog-filter">Filter good dogs: OFF</button>
       </div>
-      <div id="dog-bar"></div>
+      <div id="dog-bar">
+
+
+      </div>
       <div id="dog-summary-container">
         <h1>DOGGO:</h1>
         <div id="dog-info"></div>
